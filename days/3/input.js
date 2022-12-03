880fdc6a5d16f685d438b9e3a054100d87b07865
Vincent Hardouin <vincenthardouin@icloud.com>
2022-12-03 09:04:47 +0100

    day3:init

---
+++ days/3/input.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+function getInput() {
+  return parseInputString(fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').toString());
+}
+
+function parseInputString(input) {
+  return input
+    .trim()
+    .split('\n')
+    .map((value) => {
+      return value;
+    });
+}
+
+module.exports = {
+  getInput,
+  parseInputString,
+};
---
+++ days/3/part-two.test.js
@@ -0,0 +1,16 @@
+const { describe, it, expect } = require('@jest/globals');
+const partTwo = require('./part-two');
+const { parseInputString } = require('./input');
+
+describe('day3 | part-two', () => {
+  describe('#getResult', () => {
+    it('should ', () => {
+      const inputString = ``;
+      const input = parseInputString(inputString);
+
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe();
+    });
+  });
+});
