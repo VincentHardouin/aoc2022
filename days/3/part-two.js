e4417dc07dc1cc043b3a17ecc974aea665d3b4e2
Vincent Hardouin <vincenthardouin@icloud.com>
2022-12-03 09:45:32 +0100

    day3:part2

--- days/3/part-two.js
+++ days/3/part-two.js
@@ -1,7 +1,23 @@
+const _ = require('lodash');
 const { getInput } = require('./input');
+const { ALPHABET } = require('./constants');
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const groups = _.chunk(input, 3);
+  return groups
+    .map((group) => getShareItem(group[0], group[1], group[2]))
+    .reduce((acc, val) => acc + ALPHABET.indexOf(val) + 1, 0);
+}
+
+function getShareItem(first, second, third) {
+  for (const item of first) {
+    if (second.includes(item) && third.includes(item)) {
+      return item;
+    }
+  }
+}
 
 module.exports = {
   getResult,
+  getShareItem,
 };
--- days/3/part-two.test.js
+++ days/3/part-two.test.js
@@ -1,16 +1,35 @@
 const { describe, it, expect } = require('@jest/globals');
 const partTwo = require('./part-two');
 const { parseInputString } = require('./input');
+const partOne = require("./part-one");
 
 describe('day3 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``;
+      const inputString = `
+      vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
       const input = parseInputString(inputString);
 
       const result = partTwo.getResult(input);
 
-      expect(result).toBe();
+      expect(result).toBe(70);
+    });
+  });
+
+  describe('#getShareItem', () => {
+    it('should return share item', () => {
+      const first = 'vJrwpWtwJgWrhcsFMMfFFhFp'.split('');
+      const second = 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'.split('');
+      const third = 'PmmdzqPrVvPwwTWBwg'.split('');
+
+      const result = partOne.getShareItem(first, second, third);
+
+      expect(result).toBe('r');
     });
   });
 });
