27b96c55c110f074bb88a47fb75fb7f58fa95a37
Vincent Hardouin <vincenthardouin@icloud.com>
2022-12-06 23:45:03 +0100

    day7:init

---
+++ days/7/index.js
@@ -0,0 +1,4 @@
+const partOne = require('./part-one');
+const partTwo = require('./part-two');
+
+module.exports = [partOne.getResult, partTwo.getResult];
---
+++ days/7/part-one.test.js
@@ -0,0 +1,16 @@
+const { describe, it, expect } = require('@jest/globals');
+const partOne = require('./part-one');
+const { parseInputString } = require('./input');
+
+describe('day7 | part-one', () => {
+  describe('#getResult', () => {
+    it('should ', () => {
+      const inputString = ``;
+      const input = parseInputString(inputString);
+
+      const result = partOne.getResult(input);
+
+      expect(result).toBe();
+    });
+  });
+});
