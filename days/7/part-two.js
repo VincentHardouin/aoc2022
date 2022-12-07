e52f5ae357fbeb3465019f4bf11e9c4b12f2842a
Vincent Hardouin <vincenthardouin@icloud.com>
2022-12-07 23:48:24 +0100

    day7:part2

--- days/7/part-two.js
+++ days/7/part-two.js
@@ -1,6 +1,49 @@
 const { getInput } = require('./input');
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const tree = {};
+  let currentPath = '';
+  input.forEach((line) => {
+    if (isCurrentDirectoryCommand(line)) {
+      const currentDirectory = line.split(' ')[2];
+      currentPath =
+        currentDirectory == '..' ? currentPath.split('.').slice(0, -1).join('.') : `${currentPath}.${currentDirectory}`;
+      tree[currentPath] = tree[currentPath] ?? 0;
+    } else if (!isListCommand(line)) {
+      const object = line.split(' ');
+      if (object[0] !== 'dir') {
+        tree[currentPath] += Number(object[0]);
+      }
+    }
+  });
+
+  Object.keys(tree).forEach((key) => {
+    Object.keys(tree).forEach((key2) => {
+      if (key !== key2) {
+        if (key2.includes(key)) {
+          tree[key] += tree[key2];
+        }
+      }
+    });
+  });
+
+  const fileSystemActualFreeSpace = 70000000 - tree['./'];
+  const spaceToFreeUp = 30000000 - fileSystemActualFreeSpace;
+
+  const total = Object.entries(tree)
+    .filter(([key, val]) => key.includes('.') || key === '/')
+    .filter(([key, val]) => val >= spaceToFreeUp)
+    .sort(([, v1], [, v2]) => v1 - v2)[0][1];
+  return total;
+}
+
+function isCurrentDirectoryCommand(line) {
+  return line.match(/^\$\scd/);
+}
+
+function isListCommand(line) {
+  return line.match(/^\$\sls/);
+}
 
 module.exports = {
   getResult,
--- days/7/part-two.test.js
+++ days/7/part-two.test.js
@@ -5,12 +5,34 @@ const { parseInputString } = require('./input');
 describe('day7 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``;
+      const inputString = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`;
       const input = parseInputString(inputString);
 
       const result = partTwo.getResult(input);
 
-      expect(result).toBe();
+      expect(result).toBe(24933642);
     });
   });
 });
