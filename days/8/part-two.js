e11a3b29893af50de64c2ba86202a0b56bb527a7
Vincent Hardouin <vincenthardouin@icloud.com>
2022-12-12 12:28:47 +0100

    day8:part2

--- days/8/part-two.js
+++ days/8/part-two.js
@@ -1,7 +1,66 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
 
-function getResult(input = getInput()) {}
+function getResult(input = getInput()) {
+  const scores = [];
+  for (let i = 1; i < input.length - 1; i++) {
+    for (let j = 1; j < input[0].length - 1; j++) {
+      scores.push(getScenicScore(input, i, j));
+    }
+  }
+  return _.max(scores);
+}
+
+function getScenicScore(trees, i, j) {
+  const tree = trees[i][j];
+  // Top
+  let top = 0;
+  for (let l = i - 1; l >= 0; l--) {
+    const current = trees[l][j];
+    if (current < tree) {
+      top++;
+    } else {
+      top++;
+      break;
+    }
+  }
+
+  // Bottom
+  let bottom = 0;
+  for (let l = i + 1; l < trees.length; l++) {
+    if (trees[l][j] < tree) {
+      bottom++;
+    } else {
+      bottom++;
+      break;
+    }
+  }
+
+  // Left
+  let left = 0;
+  for (let k = j - 1; k >= 0; k--) {
+    if (trees[i][k] < tree) {
+      left++;
+    } else {
+      left++;
+      break;
+    }
+  }
+
+  // Right
+  let right = 0;
+  for (let k = j + 1; k < trees[0].length; k++) {
+    if (trees[i][k] < tree) {
+      right++;
+    } else {
+      right++;
+      break;
+    }
+  }
+  return top * bottom * left * right;
+}
 
 module.exports = {
   getResult,
+  getScenicScore,
 };
--- days/8/part-two.test.js
+++ days/8/part-two.test.js
@@ -1,16 +1,41 @@
 const { describe, it, expect } = require('@jest/globals');
 const partTwo = require('./part-two');
 const { parseInputString } = require('./input');
+const partOne = require('./part-one');
 
 describe('day8 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const inputString = ``;
+      const inputString = `30373
+25512
+65332
+33549
+35390`;
       const input = parseInputString(inputString);
 
       const result = partTwo.getResult(input);
 
-      expect(result).toBe();
+      expect(result).toBe(8);
+    });
+  });
+
+  describe('#getScenicScore', () => {
+    [
+      { i: 1, j: 2, expectedResult: 4 },
+      { i: 3, j: 2, expectedResult: 8 },
+    ].forEach(({ i, j, expectedResult }) => {
+      it(`should be ${expectedResult} when i : ${i}  and j : ${j}`, () => {
+        const inputString = `30373
+25512
+65332
+33549
+35390`;
+        const input = parseInputString(inputString);
+
+        const result = partTwo.getScenicScore(input, i, j);
+
+        expect(result).toBe(expectedResult);
+      });
     });
   });
 });
